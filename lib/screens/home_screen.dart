9840ccccd73a78ed212aad7755ab89433f6ce90d
yara30999 <yaranaserelniny@gmail.com>
2025-01-15 23:50:28 +0200

    get employees data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sprints_dio_task/models/employee.dart';
+import 'package:sprints_dio_task/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,25 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Employee> employees = [];
+
+  final ApiService service = ApiService();
+
+  @override
+  void initState() {
+    super.initState();
+    getEmployeeData();
+  }
+
+  Future<void> getEmployeeData() async {
+    try {
+      final data = await service.getEmployees();
+      setState(() {
+        employees = data;
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/services/api_service.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:sprints_dio_task/models/employee.dart';
+
+class ApiService {
+  final Dio _dio = Dio();
+  final String _url = 'https://jsonplaceholder.typicode.com/users';
+
+  Future<List<Employee>> getEmployees() async {
+    try {
+      final response = await _dio.get(_url);
+      if (response.statusCode == 200) {
+        final List<dynamic> data = response.data;
+        final employees = data.map((json) => Employee.fromJson(json)).toList();
+        return employees;
+      } else {
+        throw Exception('Failed to load employees');
+      }
+    } catch (e) {
+      throw Exception('Error fetching employees: $e');
+    }
+  }
+}
